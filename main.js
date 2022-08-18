fb676873731a968cadf5b10d91ce9f220da88e0e
Pete982 <84607563+Pete982@users.noreply.github.com>
2022-08-18 16:10:25 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+nose_x = 0;
+
+nose_y = 0;
+
+function preload(){
+
+    Mustache = loadImage("https://i.postimg.cc/W4FmRm1j/Mustache.png");
+    
+     }
+     
 function setup(){
     canvas = createCanvas(300, 300);
     canvas.center();
@@ -7,19 +17,23 @@ function setup(){
     poseNet = ml5.poseNet(video, modelloaded);
     poseNet.on('pose', gotPoses); //Pose contains all the 17 different given body parts. gotPoses will help us to only get the x and y coordinates of a certain part of the body, in this case.. a mustache. We only need a mustache for this project.//
  }
+
+ function modelloaded(){
+    console.log("Pose Net is Initialized For you Already!")
+}
  
  function gotPoses(results){
      if(results.length > 0){
          console.log(results);  //console.log(results) specifically puts and consoles the results on the screen and in the console of the code.//
  
-         console.log("Mustache x = "+results[0].pose.mustache.x); //OF THE 17 POSES OF BODY PARTS pose.mustache.x uses SPECIFICALLY and ONLY the nose part of the body for the x coordinate. Same goes for pose.mustache.y for the y coordinate.//
+         console.log("Mustache x = "+results[0].pose.nose.x); //OF THE 17 POSES OF BODY PARTS pose.mustache.x uses SPECIFICALLY and ONLY the nose part of the body for the x coordinate. Same goes for pose.mustache.y for the y coordinate.//
          
-         console.log("Mustache y = "+results[0].pose.mustache.y)  //results[0] gives the highest priority to a specific part of the body and keeps it at the top. 0 is the first place (starts with 0 and first thing it will detect is 0) that will only work when it sees a face on the screen.//
+         nose_x = results[0].pose.nose.x - 30;
+
+         console.log("Mustache y = "+results[0].pose.nose.y)  //results[0] gives the highest priority to a specific part of the body and keeps it at the top. 0 is the first place (starts with 0 and first thing it will detect is 0) that will only work when it sees a face on the screen.//
+
+         nose_y = results[0].pose.nose.y;
      }
- }
- 
- function modelloaded(){
-     console.log("Pose Net is Initialized For you Already!")
  }
  
  function take_snapshot(){
@@ -28,9 +42,7 @@ function setup(){
  
  function draw(){
      image(video, 0, 0, 300, 300); //The first 0 is the x coordinate and the second is the y. 300, 300 is the height and the width numbers.// 
+     image(Mustache, nose_x, nose_y, 80, 35);
  }
  
- function preload(){
-
- }
  
